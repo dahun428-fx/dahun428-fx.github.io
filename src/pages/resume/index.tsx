b5d1da8167d234c1154280da66c5ce210bef448f
JungDahun <dahun428@naver.com>
2024-03-21 15:17:52 +0900

    cccc

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 
 const ResumePage: NextPage = () => {
-  return <div>resume page 111</div>;
+  return <div>resume page 11111</div>;
 };
 
 export default ResumePage;
