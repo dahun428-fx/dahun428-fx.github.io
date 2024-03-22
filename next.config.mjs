462175fb86a4fed1fadb9cd4a098973647c9098b
JungDahun <dahun428@naver.com>
2024-03-23 00:57:43 +0900

    ch

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const debug = process.env.NODE_ENV !== "production";
-// const repository = "resume";
+const repository = "resume";
 const nextConfig = {
   reactStrictMode: true,
-  //   assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+    assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
   //   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
   output: "export",
-  basePath: !debug ? "/resume" : "",
+  basePath: !debug ? `/${repository}` : "",
 };
 
 export default nextConfig;
