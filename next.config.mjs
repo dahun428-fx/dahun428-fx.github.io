0fdacadf89b5f0aab6cc4bdd7c8090e8f73f9056
JungDahun <dahun428@naver.com>
2024-03-23 23:50:14 +0900

    chanage

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 const debug = process.env.NODE_ENV !== "production";
-const repository = "resume";
+// const repository = "resume";
+const homepage  = "https://dahun428-fx.github.io/resume";
+// const { homepage } = require('./package.json');
+const { pathname } = new URL(homepage);
 const nextConfig = {
   reactStrictMode: true,
-    assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  assetPrefix: !debug ? `/${pathname}/` : "", // production 일때 prefix 경로
   //   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
   output: "export",
-  basePath: !debug ? `/${repository}` : "",
+  // basePath: !debug ? `/${pathname}` : "",
 };
  
 export default nextConfig;
